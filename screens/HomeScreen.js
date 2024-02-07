b53ee2aefcb1652440fa24cbd023b0f1e0cbcadd
P-MBD <112797204+P-MBD@users.noreply.github.com>
2024-02-07 13:03:55 +0330

    Select Notes from SQLiteُ

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,40 @@
-import React, {component} from 'react';
-import {View, Text, Button} from 'react-native';
+import React, {Component ,useEffect, useState } from 'react'
+import SQLite from 'react-native-sqlite-2'
+import {View, Text, Button, FlatList} from 'react-native';
+const db = SQLite.openDatabase('note.db', '1.0', '', 1) 
 function HomeScreen({ navigation }) {
+
+  const [myItems, setItems] = useState('');
+  var data= [];
+    useEffect(()=>{
+      db.transaction(function(trx){
+        trx.executeSql("select * from tbl_note",[],function(tx, res){
+          console.log(res.rows);
+          for(let i=0; i<res.rows.length; i++){
+              var id =res.rows.item(i).id;
+              var title=res.rows.item(i).title;
+              var description = res.rows.item(i).description;
+              var dateNote = res.rows.item(i).dateNote;
+              var timeNote = res.rows.item(i).timeNote;
+              data.push({id, title, description, dateNote, timeNote})
+          }
+          setItems(data);
+        })
+      })},[])
+
+const renderItem=({item})=>(
+  <View>
+      <Text>{item.title}</Text>
+  </View>
+)
+
+
     return (
       <View >
-        <Text>Home Screen</Text>
-      <Button title="Dashboard" onPress={()=>{navigation.navigate('ProfileStack')}} />
+             <FlatList
+             data={myItems}
+             renderItem={renderItem}
+             />
       </View>
     );
   }
